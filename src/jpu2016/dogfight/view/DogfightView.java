e8a99292975bf00d63490a43e2088a675a3f39f9
ThomasDicarlo <45856930+ThomasDicarlo@users.noreply.github.com>
2019-05-20 15:43:57 +0200

    gtuvyuyg

--- src/jpu2016/dogfight/view/DogfightView.java
+++ src/jpu2016/dogfight/view/DogfightView.java
@@ -1,4 +1,21 @@
 package jpu2016.dogfight.view;
 
+import jpu2016.dogfight.controller.IOrderPerformer;
+import jpu2016.dogfight.model.IDogfightModel;
+
+import java.util.Observable;
+
 public class DogfightView {
+
+    public DogfightView(IOrderPerformer orderPerformer, IDogfightModel dogfightModel, Observable observable){}
+
+    private void run(){}
+
+    public displayMessage(String message)
+    {
+        System.out.println(" test");
+    }
+    public void closeALL(){}
+
+
 }
